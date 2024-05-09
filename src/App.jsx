b1e60b683cdc549704b3a8550a44e429d48c7c2c
arbiterkdh <arbiterkdh@gmail.com>
2024-05-09 16:24:42 +0900

    배열 map 메소드로 배열의 각 원소를 컴포넌트로 매핑 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const foodList = ["pizza", "burger", "noodle", "coffee"];
+
+  return (
+    <div>
+      {/*
+     <ul>
+      <li> pizza </li>
+      <li> burger </li>
+      <li> noodle </li>
+      <li> coffee </li>
+     </ul>
+     */}
+      <div>
+        {foodList.map((food) => (
+          <li>{food}</li>
+        ))}
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
