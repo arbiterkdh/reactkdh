32e85c84906012dee32c0e7b77841f56788368df
arbiterkdh <arbiterkdh@gmail.com>
2024-05-09 15:22:06 +0900

    javascript object와 property 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 
 function App(props) {
-  const obj = {
-    age: 30,
-    name: "John Doe",
-    email: "john@example.com",
-    password: "1234567890",
+  const son = {
+    lastName: "손",
+    firstName: "흥민",
+    address: "런던",
+    country: "영국",
+    married: false,
   };
 
   return (
     <div>
-      <p>
-        {obj.name} is {obj.age} years old.
-      </p>
-      <p>
-        And his email address is {obj.email}, password is {obj.password}.
-      </p>
+      <ul>
+        <li>성 : {son.lastName}</li>
+        <li>이름 : {son.firstName}</li>
+        <li>주소 : {son.address}</li>
+        <li>국가 : {son.country}</li>
+        <li>결혼여부 : {son.married ? "기혼" : "미혼"}</li>
+      </ul>
     </div>
   );
 }
