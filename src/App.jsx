0312b80bb87aeb4b034c73b7fb2c2800f4f8a3f0
arbiterkdh <arbiterkdh@gmail.com>
2024-05-09 14:16:12 +0900

    && || 연산자 사용법 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 function App() {
-  return <></>;
+  const name = "강인";
+  const age = 5; // or 30;
+
+  return (
+    <>
+      <p>{name}</p>
+      {name == "흥민" && <p>공격수</p>}
+      {name == "강인" && <p>미드필더</p>}
+
+      {/*  age 가 20 보다 크거나 같으면
+    <p> 투표 가능 </p>
+    age 가 20 보다 작으면
+    <p> 투표 불가능 </p>
+    */}
+      {age >= 20 && <p> 투표 가능 </p>}
+      {age < 20 && <p> 투표 불가능 </p>}
+      {age >= 20 || <p> 투표 불가능 </p>}
+      {age < 20 || <p> 투표 가능 </p>}
+    </>
+  );
 }
 
 export default App;
