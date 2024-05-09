48e60d5d04f060bfbd75b3afc72e7e04d0815dc2
arbiterkdh <arbiterkdh@gmail.com>
2024-05-09 14:30:50 +0900

    falsy, truthy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-  return null;
+  // &&, ||
+
+  // falsy(false 같은),
+  // : false, 0, "", null, undefined
+
+  const a = "undefined";
+
+  if (a) console.log("a는 참이다.");
+  else console.log("a는 거짓이다.");
+
+  // truthy(true 같은) : falsy 가 아니면 truthy
+
+  // &&, || 와 조합
+
+  let b = 0;
+  let c = b && "값이 있음";
+  console.log("c", c);
+
+  let d = b || "값이 없음";
+  console.log("d", d);
+
+  return <></>;
 }
 
 export default App;
