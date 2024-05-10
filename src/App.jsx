fa01fad533fa7c4df1e2cdc951f1a071e3fdda14
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 15:39:10 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, someProp, otherProp }) {
-  console.log("name", name);
-  console.log("age", age);
-  console.log("someProp", someProp); // true
-  console.log("otherProp", otherProp);
-  return <div></div>;
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={30} someProp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
