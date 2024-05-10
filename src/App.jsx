3c6f78b1ff62eed79796779a46b9821d66080291
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 16:09:52 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // style props
-
-  return (
-    <div>
-      <div
-        style={{
-          color: "white",
-          padding: "10px",
-          border: "1px dashed purple",
-          textAlign: "center",
-          backgroundColor: "Orange",
-        }}
-      >
-        Lorem ipsum dolor.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
