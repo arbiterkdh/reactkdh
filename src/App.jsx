7899dbcb7162acd6d88eeabc59fe64b2de6052e8
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 16:21:57 +0900

    style props 응용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+function MyComp({ headStyle, bodyStyle }) {
+  return (
+    <div>
+      <h1 style={headStyle}>Lorem.</h1>
+      <p style={bodyStyle}>Lorem ipsum dolor.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp
+        headStyle={{
+          padding: "10px",
+          border: "1px solid black",
+          color: "red",
+        }}
+        bodyStyle={{
+          margin: "10px",
+          border: "1px dotted brown",
+          color: "teal",
+          textAlign: "center",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
