d4365c2ca69efef28a58c10fbad0175b0ed38457
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 16:45:21 +0900

    코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react";
 
-function MyComp({ headStyle, bodyStyle }) {
-  return (
-    <div>
-      <h1 style={headStyle}>Lorem.</h1>
-      <p style={bodyStyle}>Lorem ipsum dolor.</p>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp
-        headStyle={{
-          padding: "10px",
-          border: "1px solid black",
-          color: "red",
-        }}
-        bodyStyle={{
-          margin: "10px",
-          border: "1px dotted brown",
-          color: "teal",
-          textAlign: "center",
-        }}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
