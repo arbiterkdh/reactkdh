c55f9adf5c8be2058145a5fefb463f8ace5b481c
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 12:24:55 +0900

    깊은 복사를 얕은 복사 여러번으로 해결

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,29 @@ function App(props) {
   const { ...address1 } = a.address; // 얕은 복사
   c.address = address1; // 깊은 복사
 
+  // 연습: 깊은 복사
+
+  const d = {
+    company: {
+      name: "apple",
+      location: "us",
+    },
+    name: "iphone",
+    price: 300,
+  };
+
+  const { ...e } = d;
+  const { ...company1 } = d.company;
+  e.company = company1;
+
+  e.name = "galaxy";
+  e.company.name = "samsung";
+
+  console.log("d.name", d.name); // iphone
+  console.log("d.company.name", d.company.name); // apple
+  console.log("e.name", e.name); // galaxy
+  console.log("e.company.name", e.company.name); // samsung
+
   return <div></div>;
 }
 
