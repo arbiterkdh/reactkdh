20b3894a896d4167b5ac1f2be828e147bf3b4981
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 15:02:25 +0900

    props 의 값이 없을 때 기본값 할당하는 방법 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-function MyComp({ name, age, team = "무소속" }) {
+function MyButton({ name = "클릭" }) {
   return (
     <div>
-      <ul>
-        <li>이름: {name}</li>
-        <li>나이: {age}</li>
-        <li>팀: {team}</li>
-      </ul>
+      <button>{name}</button>
     </div>
   );
 }
@@ -15,9 +11,10 @@ function MyComp({ name, age, team = "무소속" }) {
 function App(props) {
   return (
     <div>
-      <MyComp name="흥민" age={33} team="토트넘" />
-      <MyComp name="강인" age={22} team="파리" />
-      <MyComp name="길동" age={44} />
+      <MyButton name="버튼1" />
+      {/* 버튼1 */}
+      <MyButton />
+      {/* 클릭 */}
     </div>
   );
 }
