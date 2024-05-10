4d8b4dfce0e2a7b205aa23e86594e19f22fe7977
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 16:17:45 +0900

    style props 응용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+function MyComp({ headColor, bodyColor }) {
+  return (
+    <div>
+      <h1 style={{ color: headColor }}>Lorem.</h1>
+      <p style={{ color: bodyColor }}>Lorem ipsum dolor.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp headColor={"firebrick"} bodyColor={"magenta"} />
+      <MyComp headColor={"green"} bodyColor={"lime"} />
+    </div>
+  );
 }
 
 export default App;
