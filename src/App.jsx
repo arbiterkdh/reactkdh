46a4a6b194cfdb05fa9db1b5a608bccb3f1a80a4
arbiterkdh <arbiterkdh@gmail.com>
2024-05-10 16:51:42 +0900

    chakra ui 맛보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <ChakraProvider>
+        <Button colorScheme={"blue"}>클릭1</Button>
+        <Button colorScheme={"orange"}>클릭2</Button>
+        <Button
+          sx={{
+            color: "olive",
+            fontSize: "1.5rem",
+            padding: "10px",
+          }}
+          colorScheme={"gray"}
+        >
+          클릭3
+        </Button>
+      </ChakraProvider>
+    </div>
+  );
 }
 
 export default App;
