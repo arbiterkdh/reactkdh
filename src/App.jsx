543686c61f2211ca331463be59dae8f90b6c20ce
arbiterkdh <arbiterkdh@gmail.com>
2024-05-13 17:00:48 +0900

    혼자 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyButton({ onClick }) {
+  return <button onClick={onClick}>클릭</button>;
+}
+
+function MyNumber({ number }) {
+  return <div>number : {number}</div>;
+}
 
 function App(props) {
-  return <div></div>;
+  const [number, setNumber] = useState(0);
+  return (
+    <div>
+      <MyButton
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      />
+      <MyNumber number={number} />
+    </div>
+  );
 }
 
 export default App;
