af3f0eee897f820d5b07a64c4aca8f628cc78a75
arbiterkdh <arbiterkdh@gmail.com>
2024-05-13 15:46:12 +0900

    state 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [hobby, setHobby] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder={"Name"}
+        />
+      </div>
+      <div>
+        <input
+          onChange={(e) => setHobby(e.target.value)}
+          type="text"
+          placeholder={"Hobby"}
+        />
+      </div>
+      <div>
+        <input
+          onChange={(e) => setAddress(e.target.value)}
+          type="text"
+          placeholder={"address"}
+        />
+      </div>
+      <div>
+        <input
+          onChange={(e) => setCity(e.target.value)}
+          type="text"
+          placeholder={"city"}
+        />
+      </div>
+      <div>
+        {name}, {hobby}, {address}, {city}
+      </div>
+    </div>
+  );
 }
 
 export default App;
