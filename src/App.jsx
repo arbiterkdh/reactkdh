0d9ed9c83dc529a99c69578877ece7a3756fa90e
arbiterkdh <arbiterkdh@gmail.com>
2024-05-13 11:12:32 +0900

    코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <div>
-      <ChakraProvider>
-        <Button colorScheme={"blue"}>클릭1</Button>
-        <Button colorScheme={"orange"}>클릭2</Button>
-        <Button
-          sx={{
-            color: "olive",
-            fontSize: "1.5rem",
-            padding: "10px",
-          }}
-          colorScheme={"gray"}
-        >
-          클릭3
-        </Button>
-      </ChakraProvider>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
