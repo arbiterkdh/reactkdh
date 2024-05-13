070b010acfc70c7a1b4847b900905eead2701f16
arbiterkdh <arbiterkdh@gmail.com>
2024-05-13 15:35:12 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
-
-  function handleText() {
-    if (text.length > 0) {
-      const [...nextList] = list;
-      nextList.push(text);
-      setList(nextList);
-      setText("");
-    }
-  }
-
-  return (
-    <div>
-      <input
-        value={text}
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleText}>추가</button>
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
