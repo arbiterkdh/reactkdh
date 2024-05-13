9fee6be7a90f2f1790df651d158464f83bd8de6b
arbiterkdh <arbiterkdh@gmail.com>
2024-05-13 16:35:08 +0900

    하나의 상태가 여러 property 를 갖는 경우 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [car, setCar] = useState({
+    name: "",
+    model: "",
+    company: "",
+    price: "",
+  });
+
+  function handleChange(e) {
+    const { ...nextCar } = car;
+    nextCar[e.target.name] = e.target.value;
+    setCar(nextCar);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          name={"name"}
+          onChange={handleChange}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name={"model"}
+          onChange={handleChange}
+          placeholder={"모델"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name={"company"}
+          onChange={handleChange}
+          placeholder={"회사"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name={"price"}
+          onChange={handleChange}
+          placeholder={"가격"}
+        />
+      </div>
+
+      <ul>
+        <li>이름: {car.name}</li>
+        <li>모델: {car.model}</li>
+        <li>회사: {car.company}</li>
+        <li>가격: {car.price}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
