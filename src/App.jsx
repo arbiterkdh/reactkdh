948eec617d4756b43eefe543e9120549ee900e73
arbiterkdh <arbiterkdh@gmail.com>
2024-05-13 15:05:07 +0900

    state 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  const isTooLong = text.length > 10;
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <p>{text}</p>
+      {isTooLong && <p>11자 이상 입력할 수 없습니다.</p>}
+    </div>
+  );
 }
 
 export default App;
