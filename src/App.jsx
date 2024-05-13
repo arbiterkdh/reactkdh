371e49d2e545b7efd7cf1081bbb2421d5f94dc2a
arbiterkdh <arbiterkdh@gmail.com>
2024-05-13 11:32:54 +0900

    react 컴포넌트 상태
    컴포넌트는 상태를 가지고 있음
    상태가 변경되면 다시 그려짐
    상태는 react 가 관리함

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  let val = 0;
+  function increment() {
+    val++;
+  }
+  return (
+    <div>
+      {val}
+      <button onClick={increment}>증가</button>
+    </div>
+  );
+}
+
+function MyComp2() {
+  const [val, setVal] = useState(0);
+  function increment() {
+    setVal(val + 1);
+  }
+  return (
+    <div>
+      {val}
+      <button onClick={increment}>증가</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+      <MyComp2 />
+    </div>
+  );
 }
 
 export default App;
