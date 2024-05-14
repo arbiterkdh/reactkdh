74678dc83876bf05b282f46aea9fd62ab0c33e51
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 11:43:47 +0900

    혼자 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+
+const MessageContext = createContext("");
+
+function MyBox() {
+  const message = useContext(MessageContext);
+  return <div>{message}</div>;
+}
+
+function MySection() {
+  return <MyBox />;
+}
+
+function MyContainer() {
+  return <MySection />;
+}
 
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState("");
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setMessage(e.target.value)} />
+      <p>{message}</p>
+      <hr />
+      <MessageContext.Provider value={message}>
+        <MyContainer />
+      </MessageContext.Provider>
+    </div>
+  );
 }
 
 export default App;
