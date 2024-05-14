bb31a9ab94120a3f9bb86cbeb03d53445db35333
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 15:08:55 +0900

    useEffect 의 첫번째 파라미터에서 dependency 를 변경하지 않도록 주의

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("mount 될 때");
+  }, []);
+
+  useEffect(() => {
+    console.log("count 가 변경될 때");
+    // dependency 가
+    // 이 함수 내에서 변경되지 않도록 주의
+    // 예) setCount(count + 1);
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>click</button>
+    </div>
+  );
 }
 
 export default App;
