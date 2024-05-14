dc803b82fa089a43b41a402bd5c975797760e746
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 09:30:26 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <div>{count}</div>
-      <div>
-        <button onClick={() => setCount(count + 1)}>UP</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-      <hr />
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
