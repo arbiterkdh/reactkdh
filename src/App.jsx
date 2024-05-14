1905d29c0d1b74332b21632e5f265f2cc5fd5f55
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 12:30:02 +0900

    컨텍스트로 데이터 (객체) 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+
+// step1 : 컨텍스트 만들기
+const TextContext = createContext(null);
+
+function MyInput() {
+  // step2 : 컨텍스트 사용하기
+  const textHandler = useContext(TextContext);
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => textHandler.updateText(e.target.value)}
+      />
+      <p>{textHandler.text}</p>
+    </div>
+  );
+}
+
+function MyText() {
+  const textHandler = useContext(TextContext);
+
+  return (
+    <div>
+      <p>{textHandler.text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  function handleUpdateText(e) {
+    setText(e);
+  }
+
+  return (
+    <div>
+      {/* step3 : 컨텍스트 제공하기 */}
+      <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
+        <MyInput />
+        <MyText />
+      </TextContext.Provider>
+    </div>
+  );
 }
 
 export default App;
