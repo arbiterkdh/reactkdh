46f2f988df04e3d593b6152c84a2f76c7ef19c36
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 09:30:01 +0900

    컴포넌트 각자 상태 연습.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox() {
+  const [val, setVal] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input type="text" onChange={(e) => setVal(e.target.value)} />
+        <p>{val}</p>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyBox />
+      <hr />
+      <MyBox />
+    </div>
+  );
 }
 
 export default App;
