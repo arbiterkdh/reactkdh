207f91a30428e5e8daf1584a83f9f30c761e7d39
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 09:13:04 +0900

    혼자 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyInput() {
+  function handleChange(e) {
+    const { ...nextFavorite } = favorite;
+  }
+
+  return (
+    <div>
+      <div>
+        <input type="text" name={"name"} onChange={handleChange} />
+      </div>
+      <div>
+        <input type="text" name={"singer"} onChange={handleChange} />
+      </div>
+    </div>
+  );
+}
+
+function MyButton() {
+  return <button>등록</button>;
+}
+
+function MyList() {
+  return <ul></ul>;
+}
 
 function App(props) {
-  return <div></div>;
+  const [favorite, setFavorite] = useState({
+    name: "",
+    singer: "",
+  });
+  const [list, setList] = useState([]);
+  return (
+    <div>
+      <MyInput favorite={favorite} />
+      <MyButton />
+      <MyList />
+    </div>
+  );
 }
 
 export default App;
