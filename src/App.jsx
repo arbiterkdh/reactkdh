3198e47f605694f833ca1774e21f42018c9fbdf5
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 15:09:48 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("mount 될 때");
-  }, []);
-
-  useEffect(() => {
-    console.log("count 가 변경될 때");
-    // dependency 가
-    // 이 함수 내에서 변경되지 않도록 주의
-    // 예) setCount(count + 1);
-  }, [count]);
-
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>click</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
