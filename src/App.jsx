37bef1565bf5d6da0e796c45092dafe88da33c46
arbiterkdh <arbiterkdh@gmail.com>
2024-05-14 12:39:47 +0900

    컨텍스트로 데이터 (객체) 전달 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+
+const TextContext = createContext(null);
+
+function MyInput() {
+  const textHandler = useContext(TextContext);
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => textHandler.updateText(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function MyText() {
+  const textHandler = useContext(TextContext);
+  return (
+    <div>
+      <p>{textHandler.text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  function handleUpdateText(t) {
+    setText(t);
+  }
+
+  return (
+    <div>
+      <TextContext.Provider
+        value={{
+          text: text,
+          updateText: handleUpdateText,
+        }}
+      >
+        <MyInput />
+        <MyText />
+      </TextContext.Provider>
+    </div>
+  );
 }
 
 export default App;
