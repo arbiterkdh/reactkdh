1d6f77d7daa8e5f6a17898bb3a8d5afb737138a8
arbiterkdh <arbiterkdh@gmail.com>
2024-05-16 16:05:29 +0900

    200 외 응답은 catch 메소드의 파라미터(함수) 가 일함

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick200() {
+    axios.get("api/main43/sub1").then(() => {
+      console.log("200 응답");
+    });
+  }
+
+  function handleClick400() {
+    axios
+      .get("api/main43/sub2")
+      .then(() => {
+        console.log("400 응답");
+      })
+      .catch(() => console.log("catch 의 메소드, 400 응답시"));
+  }
+
+  function handleClick404() {
+    axios.get("/api/main43/sub5").catch(() => console.log("404 캐치"));
+  }
+
+  function handleClick500() {
+    axios.get("/api/main43/sub6").catch(() => console.log("500 캐치"));
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick200}>200 응답</button>
+      <button onClick={handleClick400}>400 응답</button>
+      <button onClick={handleClick404}>404 응답</button>
+      <button onClick={handleClick500}>500 응답</button>
+    </div>
+  );
 }
 
 export default App;
