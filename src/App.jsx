bc96b94b77e99a734cc8316af68527a59b5c4807
arbiterkdh <arbiterkdh@gmail.com>
2024-05-16 10:35:55 +0900

    /api/some-url4 의 get 요청 버튼 작성 연습

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,33 @@ function App(props) {
     axios.get(`/api/some-url3?${params}`);
   }
 
+  function handleClickButton6() {
+    const params = new URLSearchParams();
+    params.append("id", 5);
+    params.append("city", "서울");
+    params.append("country", "한국");
+
+    axios.post("/api/some-url3", params);
+  }
+
+  function handleClickButton7() {
+    const params = new URLSearchParams();
+    params.append("id", 5);
+    params.append("name", "son");
+    params.append("email", "son@son.com");
+
+    axios.get(`/api/some-url4?${params}`);
+  }
+
+  function handleClickButton8() {
+    const params = new URLSearchParams();
+    params.append("id", 7);
+    params.append("city", "London");
+    params.append("country", "England");
+
+    axios.post("/api/some-url4", params);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
@@ -45,8 +72,13 @@ function App(props) {
       {/* /api/some-url2 */}
       <button onClick={handleClickButton3}>get 요청2</button>
       <button onClick={handleClickButton4}>post 요청2</button>
-
+      {/* data 도 함께 보내기 */}
       <button onClick={handleClickButton5}>get 요청 with query string</button>
+      <button onClick={handleClickButton6}>post 요청 with data</button>
+
+      {/* /api/some-url4 get, post with data */}
+      <button onClick={handleClickButton7}>get 요청 with data</button>
+      <button onClick={handleClickButton8}>post 요청 with data</button>
 
       {/*  혼자 연습용 */}
       <button onClick={handleClickButtonPractice1}>get practice</button>
