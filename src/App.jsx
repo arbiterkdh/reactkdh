9f9fe5331c57cdf700ebcd8edabc11e38d76775f
arbiterkdh <arbiterkdh@gmail.com>
2024-05-17 14:26:29 +0900

    react router 로 경로별 다른 컴포넌트 보여주기 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    page: "about",
+    element: <div>about</div>,
+  },
+  {
+    page: "search",
+    element: <div>search</div>,
+  },
+  {
+    page: "home",
+    element: <div>home</div>,
+  },
+]);
+// /about
+// /search
+// /home
+// / 위 경로에 다른 컴포넌트가 출력되도록 작성
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
