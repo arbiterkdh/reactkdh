2ad61c019bd0eb120d025e5a39d93f8e61ec5908
arbiterkdh <arbiterkdh@gmail.com>
2024-05-17 15:48:51 +0900

    index 경로 연습

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,34 @@ import {
 
 function ReactRoot() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row-reverse",
+        gap: "20px",
+      }}
+    >
       <div
         style={{
           display: "flex",
           gap: "20px",
-          backgroundColor: "orange",
-          height: "50px",
+          backgroundColor: "linen",
           fontSize: "2rem",
+          flexDirection: "column",
+          height: "800px",
         }}
       >
         <div>
-          <Link className={"link"} to={"/react/doc"}>
-            DOC
-          </Link>
+          <Link to={"/react"}>HOME</Link>
         </div>
         <div>
-          <Link className={"link"} to={"/react/tutorial"}>
-            TUTORIAL
-          </Link>
+          <Link to={"/react/doc"}>DOC</Link>
         </div>
         <div>
-          <Link className={"link"} to={"/react/sample"}>
-            SAMPLE
-          </Link>
+          <Link to={"/react/tutorial"}>TUTORIAL</Link>
+        </div>
+        <div>
+          <Link to={"/react/sample"}>SAMPLE</Link>
         </div>
       </div>
       <div>
@@ -46,6 +50,7 @@ const router = createBrowserRouter([
     path: "react",
     element: <ReactRoot />,
     children: [
+      { index: true, element: <div>react main page</div> },
       { path: "doc", element: <div>react doc</div> },
       { path: "tutorial", element: <div>react tutorial</div> },
       { path: "sample", element: <div>react sample</div> },
