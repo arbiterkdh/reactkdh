3401c940cdf4c0522b722970bfce3dd24c1d063a
arbiterkdh <arbiterkdh@gmail.com>
2024-05-17 17:01:31 +0900

    혼자 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+  useParams,
+} from "react-router-dom";
+
+function Root() {
+  return (
+    <div>
+      <div>
+        <div>
+          <Link to={"/"}>MAIN</Link>
+        </div>
+        <div>
+          <Link to={"/list"}>LIST</Link>
+        </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+function BoardList() {
+  const params = useParams();
+  return (
+    <div>
+      <div>1번 게시물</div>
+      <div>2번 게시물</div>
+      <div>3번 게시물</div>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <div>main page</div>,
+      },
+      {
+        path: "board",
+        element: <BoardView />,
+      },
+      {
+        path: "list",
+        element: <BoardList />,
+      },
+    ],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
