c4afee7db328e6f2b94f7d9a18ff6c3383ae697c
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 11:27:12 +0900

    모달 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Button,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  return (
+    <ChakraProvider>
+      <Button onClick={onOpen}>모달 오픈!</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>TITLE</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Lorem ipsum dolor sit amet.</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>CLOSE</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </ChakraProvider>
+  );
 }
 
 export default App;
