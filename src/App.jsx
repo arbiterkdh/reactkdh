230b2e8e2367e39d0f0215827910c759d4f7b23a
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 10:26:09 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Button sx={{ color: "orange", bgColor: "black" }} colorScheme={"blue"}>
-        Lorem.
-      </Button>
+      <Box></Box>
     </ChakraProvider>
   );
 }
