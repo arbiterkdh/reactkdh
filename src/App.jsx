8ac4d0bf5cdd6f2ae5db9220b2d72ed86101d804
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 11:37:59 +0900

    토스트 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, Button, ChakraProvider, useToast } from "@chakra-ui/react";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  const toast = useToast();
+  return (
+    <ChakraProvider>
+      <Button
+        onClick={() => {
+          toast({
+            title: "제목",
+            description: "내용",
+            status: "warning",
+            duration: 1000,
+            isClosable: true,
+            position: "bottom-left",
+            render: () => (
+              <Box color={"white"} p={3} bg={"blue.500"}>
+                Hello world
+              </Box>
+            ),
+          });
+        }}
+      >
+        Toast
+      </Button>
+    </ChakraProvider>
+  );
 }
 
 export default App;
