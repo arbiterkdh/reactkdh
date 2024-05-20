368a7930243c8c08fc1c67ca6bf4943fdca12eeb
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 14:33:46 +0900

    기본 동작 막기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function handleLinkClick(e) {
+    e.preventDefault();
+  }
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log("다른 동작하는 코드 작성");
+  }
+
+  return (
+    <div>
+      <a href="https://www.naver.com" onClick={handleLinkClick}>
+        네이버
+      </a>
+      <hr />
+      <form action="https://www.naver.com" onSubmit={handleSubmitForm}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
