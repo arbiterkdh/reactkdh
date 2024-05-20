1e8614795a3942e6a906b10f002aca346b7ab571
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 10:06:40 +0900

    스페이스
    https://v2.chakra-ui.com/docs/styled-system/theme#spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <div></div>
+      <Box m={1} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={2} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={10} bgColor={"tomato"}>
+        Dolorem, hic totam.
+      </Box>
+      <Box m={20} bgColor={"tomato"}>
+        Consequatur, exercitationem, voluptates.
+      </Box>
+      <Box bgColor={"tomato"}>Autem, iusto, sed!</Box>
+      <Box bgColor={"tomato"}>Commodi, cumque tempore.</Box>
     </ChakraProvider>
   );
 }
