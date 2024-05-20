ddfce433bb64ffae47f622cf10130c71277e462c
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 11:41:43 +0900

    chakra ui icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { AddIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button>
+          <AddIcon />
+        </Button>
+        <Button leftIcon={<EmailIcon />}>email</Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
