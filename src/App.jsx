c81fc0c0067615f59120f60e56520572ccc74c67
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 10:07:56 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Box m={1} bgColor={"tomato"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box m={2} bgColor={"tomato"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box m={10} bgColor={"tomato"}>
-        Dolorem, hic totam.
-      </Box>
-      <Box m={20} bgColor={"tomato"}>
-        Consequatur, exercitationem, voluptates.
-      </Box>
-      <Box bgColor={"tomato"}>Autem, iusto, sed!</Box>
-      <Box bgColor={"tomato"}>Commodi, cumque tempore.</Box>
+      <div></div>
     </ChakraProvider>
   );
 }
