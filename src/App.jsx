82e0053734b2c52f2e7cffc458a1db22ed21806b
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 11:18:42 +0900

    모달

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Button,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  return (
+    <ChakraProvider>
+      <Button onClick={onOpen}>모달 오픈!</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>TITLE</ModalHeader>
+          <ModalBody>Lorem ipsum dolor.</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button colorScheme={"blue"}>저장</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </ChakraProvider>
+  );
 }
 
 export default App;
