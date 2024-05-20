dba4e37991f622e4aeaecad03fe189ee6fd172f1
arbiterkdh <arbiterkdh@gmail.com>
2024-05-20 12:26:21 +0900

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
-import { AddIcon, EmailIcon } from "@chakra-ui/icons";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button>
-          <AddIcon />
-        </Button>
-        <Button leftIcon={<EmailIcon />}>email</Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <ChakraProvider></ChakraProvider>;
 }
 
 export default App;
